a1c161fdcd3071a01d2137dab21360bc53530cf8
Kanishk Khurana <74613246+KanishkKhurana@users.noreply.github.com>
2022-11-13 15:13:58 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,16 @@ import '../styles/globals.css'
 import '../styles/app.css'
 import Head from 'next/head'
 import Script from 'next/script'
+import {useEffect} from 'react'
 // import satyugLogo from "../assets/images/satyugLogo.png";
 
 function MyApp({ Component, pageProps }) {
-  
+  useEffect(()=>{
+      window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-B64YMWSLWS');
+  },[])
   return (<>
   <Head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
