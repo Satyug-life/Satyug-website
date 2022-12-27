a70143b4853fee1771d912c3826e0bcdd03a5302
LordFLader <devyansh1kalra@gmail.com>
2022-12-27 14:40:52 +0530

    minor fixes

--- pages/ram-setu/contact.js
+++ pages/ram-setu/contact.js
@@ -296,7 +296,7 @@ const Contact = () => {
       })
       .catch(function (error) {
         console.log(error);
-        if (error.response.status === 500) {
+        if (error.response.status === 300) {
           console.log("wallet id from backend is " + error.response.data.userToFind.walletId)
           setcurrentAccount(error.response.data.userToFind.walletId)
           console.log("EMAIL ID Already Exists");
@@ -416,9 +416,9 @@ const Contact = () => {
               ref={buttonsubmit}
               className={`button1 ${hide}`}
               onClick={() => {
+                
                 setData();
                 setbtn(!btn);
-
                 setValidDetails(true);
                 // postingData();
                 // sendEmail();
