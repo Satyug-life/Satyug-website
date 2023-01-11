5158460990029114572be73753efadf95d9e273c
Unnat Das <udas4153@gmail.com>
2023-01-11 19:28:06 +0530

    added whatsapp feature

--- pages/ram-setu/contact.js
+++ pages/ram-setu/contact.js
@@ -40,6 +40,7 @@ const Contact = () => {
 
   // const BASE_URL = "http://localhost:8080/api/ram-setu/contact";
   // const demo = 'http://localhost:8080/api/token/karma'
+  //const whatsappshareurl = "http://localhost:8080/api/whatsapp-share";
 
   console.log(BASE_URL) ;
   axios.get(BASE_URL).then((respone) => {
@@ -111,6 +112,21 @@ const Contact = () => {
   const emailinput = useRef();
   const cross = useRef();
   const navigate = useRouter().push;
+  const sendWhatsappMessage  = async ()=>{
+    axios.post("https://backend.satyug.life/api/whatsapp-share" , {
+      name: userName,
+      phoneNumber : `91${userNumber}`
+    }).then((response)=>{
+      if(response.status === 500)
+      {
+        console.log("whatsapp not send");
+      }else{
+        console.log(response);
+      }
+    }).catch((Errr)=>{
+      console.log(Errr);
+    })
+  }
   async function handleShare() {
     let tokenData={
       walletId:currentAccount,
@@ -138,6 +154,10 @@ const Contact = () => {
   // catch(error){
   //   console.log(error);
   // }
+  }
+  function handleSkipClick() {
+    setVidOn(true);
+    setQuizCompleted(true);
   }
   async function welcomeEmail() {
     // e.preventDefault()
@@ -418,7 +438,7 @@ const Contact = () => {
               ref={noinput}
               name="form_number"
               className={`noinput ${hide}`}
-              placeholder="Phone number"
+              placeholder="Whatsapp Number"
               value={number}
               onChange={(e) => {
                 setNumber(e.target.value);
@@ -453,6 +473,7 @@ const Contact = () => {
                 setData();
                 setbtn(!btn);
                 setValidDetails(true);
+                sendWhatsappMessage();
                 // postingData();
                 // sendEmail();
               }}
@@ -713,6 +734,24 @@ const Contact = () => {
         ) : (
           <></>
         )}
+        <button
+            className="SkipButtonUniversal"
+            onClick={() =>  handleSkipClick()}
+          >
+            <div className="SkipButtonUniversalContainer">
+              Skip
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-skip-forward"
+                viewBox="0 0 16 16"
+              >
+                <path d="M15.5 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V8.752l-6.267 3.636c-.52.302-1.233-.043-1.233-.696v-2.94l-6.267 3.636C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696L7.5 7.248v-2.94c0-.653.713-.998 1.233-.696L15 7.248V4a.5.5 0 0 1 .5-.5zM1 4.633v6.734L6.804 8 1 4.633zm7.5 0v6.734L14.304 8 8.5 4.633z" />
+              </svg>
+            </div>
+          </button>
     </div>
   );
 };
